d6de15ed09afd1ac487343da29411d2a91d5d59c
Zachary Sain <zachsain@gmail.com>
2022-08-10 11:07:35 -0500

    completed

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -13,6 +13,7 @@ function MovieForm() {
     discount: false,
     female_director: false,
   });
+  const [errors, setErrors] = useState([]);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -22,9 +23,13 @@ function MovieForm() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
+    }).then((response) => {
+      if (response.ok) {
+        response.json().then((newMovie) => console.log(newMovie));
+      } else {
+        response.json().then((errorData) => setErrors(errorData.errors));
+      }
     })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
   }
 
   function handleChange(e) {
@@ -126,6 +131,14 @@ function MovieForm() {
           </label>
         </FormGroup>
         <SubmitButton type="submit">Add Movie</SubmitButton>
+        {errors.length > 0 && (
+    <ul style={{ color: "red" }}>
+      {errors.map((error) => (
+        <li key={error}>{error}</li>
+      ))}
+    </ul>
+  )}
+  <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
   );
